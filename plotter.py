fb7b026b26cb99ec5221be4ad907fe599ed35402
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-12-28 16:23:01 +1300

    plotting foo

--- plotter.py
+++ plotter.py
@@ -1,5 +1,7 @@
 import config
+import datetime
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from numba import njit
 import numpy as np
 import sqlite3
@@ -41,6 +43,30 @@ def make_plot(mode):
     ts = np.array(ts)
     ys = np.array(ys)
 
+    # Convert ts to datetimes so as to calculate good tick positions
+    datetimes = []
+    for i in range(len(ts)):
+        datetimes.append(datetime.datetime.utcfromtimestamp(ts[i]))
+
+    # Generate ticks as dates on the first of each quarter
+    ticks = [datetimes[0].replace(day=1).date()]
+    while True:
+        tick = ticks[-1] + datetime.timedelta(1)
+        while tick.day != 1 or (tick.month - 1) % 3 != 0:
+            tick += datetime.timedelta(1)
+        ticks.append(tick)
+        if tick > datetimes[-1].date():
+            break
+
+    # Compute xlim
+    xlim = [mdates.date2num(ticks[0]) - 0.5,
+            mdates.date2num(ticks[1]) + 0.5]
+    if mdates.epoch2num(ts[0]) < xlim[0]:
+        xlim[0] = mdates.epoch2num(ts[0]) - 0.5
+    if mdates.epoch2num(ts[-1]) > xlim[1]:
+        xlim[1] = mdates.epoch2num(ts[-1]) + 0.5
+
+
     # Plotting stuff
     plt.rcParams["font.family"] = "Liberation Sans"
     plt.rcParams["font.size"] = 14
@@ -48,36 +74,31 @@ def make_plot(mode):
     plt.rcParams["axes.facecolor"] = "#3c3d3c"
     plt.rcParams["savefig.facecolor"] = "#3c3d3c"
 
-    plt.figure(figsize=(15, 11))
-
+    plt.figure(figsize=(15, 12))
     plt.subplot(2, 1, 1)
-    times_in_days = (ts - 1483228800)/86400.0
-    days = times_in_days.astype("int64")
-    plt.plot(times_in_days, ys, "w-", linewidth=1.5)
+
+    plt.plot(mdates.epoch2num(ts), ys, "w-", linewidth=1.5)
+    plt.xticks([])
+    plt.xlim(xlim)
+
     plt.ylabel("{mode}".format(mode=mode))
     plt.title("Total {mode} = {n}.".format(n=ys[-1], mode=mode))
-    plt.xlim([days.min() - 1, days.max() + 1])
     plt.ylim(bottom=0.0)
     plt.gca().tick_params(labelright=True)
 
     # Add vertical lines for new years (approximately)
-    new_years = np.arange(0, 5)*365.2425
-    for year in new_years:
-        plt.axvline(year, color="r", alpha=0.8, linestyle="--")
+    for year in range(2017, 2021):
+        plt.axvline(mdates.date2num(datetime.date(year, 1, 1)),
+                    color="r", alpha=0.8, linestyle="--")
 
-    # Add text about years
-    year_names = [2017, 2018, 2019]
-    for i in range(len(year_names)):
-        year = new_years[i]
-        plt.text(year+5.0, 0.95*plt.gca().get_ylim()[1],
-                    "{text} begins".format(text=year_names[i]),
-                    fontsize=10)
-
-    # Add line and text about MH's video
-    plt.axvline(890.0, linestyle="dotted", linewidth=2, color="g")
-    plt.text(890.0, 0.2*plt.gca().get_ylim()[1],
-            "@MH video\n\'Why I Left YouTube\'\ngoes viral",
-            fontsize=10)
+    # Add text about MH's video
+    if mode == "num_channels" or mode == "num_streams":
+        loc = mdates.date2num(datetime.date(2019, 8, 1))
+        plt.axvline(loc, color="g", linestyle="--", alpha=0.7)
+        plt.text(loc,
+                 0.2*plt.gca().get_ylim()[1],
+                 "@MH video\n\'Why I Left YouTube\'\ngoes viral",
+                 fontsize=10)
 
     plt.subplot(2, 1, 2)
 
@@ -85,7 +106,7 @@ def make_plot(mode):
     # was published BEFORE that.
     color = "#6b95ef"
     thin = int(86400.0/config.interval)
-    t = times_in_days[0::thin][1:]
+    t = mdates.epoch2num(ts)[0::thin][1:]
     y = derivative(t, ys[0::thin])
 
     plt.plot(t, y, alpha=0.9, color=color, label="Raw")
@@ -94,25 +115,22 @@ def make_plot(mode):
         plt.plot(t, moving_average(y), alpha=0.9, color="w",
                     label="10-day moving average")
 
-    plt.xlim([days.min() - 1, days.max() + 1])
+    plt.xticks(mdates.date2num(ticks), ticks, rotation=70)
+    plt.xlim(xlim)
     plt.ylim(bottom=0.0)
-    plt.xlabel("Time (days since 2017-01-01)")
-    plt.ylabel("New {mode} added each day".format(mode=mode))
-#    plt.title("Recent average rate (last 30 days) = {n} {mode} per day.".\
-#                format(n=int(np.sum(ts >= ts[-1] - 30.0*86400.0)/30.0),
-#                       mode=mode))
-
+    plt.ylabel("{mode} daily change".format(mode=mode))
     plt.gca().tick_params(labelright=True)
 
-    # Year lines
-    for year in new_years:
-        plt.axvline(year, color="r", alpha=0.8, linestyle="--")
+#    # Year lines
+#    for year in new_years:
+#        plt.axvline(year, color="r", alpha=0.8, linestyle="--")
 
-    # MH line
-    plt.axvline(890.0, linestyle="dotted", linewidth=2, color="g")
+#    # MH line
+#    plt.axvline(890.0, linestyle="dotted", linewidth=2, color="g")
     plt.legend()
     plt.savefig("plots/{mode}.svg".format(mode=mode), bbox_inches="tight")
     plt.savefig("plots/{mode}.png".format(mode=mode), bbox_inches="tight")
+    plt.close("all")
     print("    Figure saved to {mode}.svg and {mode}.png.".format(mode=mode))
 
 
