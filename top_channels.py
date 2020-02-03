2f0f856bf88dc84e61da9c5148fc3c40317f91d6
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-02-04 05:50:00 +1300

    working on top channels

--- top_channels.py
+++ top_channels.py
@@ -3,7 +3,6 @@ import config
 import datetime
 import json
 import numpy as np
-import pandas as pd
 import requests
 import time
 import yaml
@@ -13,6 +12,7 @@ def channels_with_content():
     """
     Return a list of all channels with content.
     """
+    print("Finding channels with content...", end="", flush=True)
     result = []
 
     # Open claims.db
@@ -27,6 +27,7 @@ def channels_with_content():
         result.append(row[0])
 
     conn.close()
+    print("done. Found {k} channels.".format(k=len(result)))
     return result
 
 
@@ -61,4 +62,33 @@ def get_followers(channels, start, end):
     return result
 
 
+def get_top(n=200):
+    """
+    Compute the top n=200
+    """
+    channels = channels_with_content()
+    counts = []
+    for i in range(len(channels)//100 + 1):
+        counts += get_followers(channels, 100*i, 100*(i+1))
+        print(len(counts))
+
+    ii = np.argsort(counts)[::-1]
+    channels = np.array(channels)[ii]
+
+    # Put into a dict
+    result = {"ranks": [],
+              "claim_ids": [],
+              "vanity_names": []}
+
+    conn = apsw.Connection(config.claims_db_file)
+    c = conn.cursor()
+    for i in range(n):
+        result["ranks"].append(i+1)
+        result["claim_ids"].append(channels[i])
+        name = c.execute("SELECT claim_name FROM claim WHERE claim_id=?",
+                         (channels[i],)).fetchone()
+        result["vanity_names"].append(name)
+
+    conn.close()
+    return result
 
