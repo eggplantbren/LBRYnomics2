f4450d3149b2755aa83cad7cf75e839097b1e16f
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-02-12 03:48:48 +1300

    Updated special channels

--- create_db.py
+++ create_db.py
@@ -66,13 +66,14 @@ def create_db():
 
 
     # Special treatment for some claims
+
     # LBRY Inc channels
     inc = set(["f3da2196b5151570d980b34d311ee0973225a68e",
                "70b8a88fc6e5ce9e4d6e8721536688484ecd79f4",
                "3fda836a92faaceedfe398225fb9b2ee2ed1f01a",
                "e48d2b50501159034f68d53321f67b8aa5b1d771",
-               "e8fed337dc4ee260f4bcfa6d24ae1e4dd75c2fb3"])
-
+               "e8fed337dc4ee260f4bcfa6d24ae1e4dd75c2fb3",
+               "4c29f8b013adea4d5cca1861fb2161d5089613ea"])
 
     # LBRY Social channels
     ls = set(["5bd299a92e7b31865d2bb3e2313402edaca41a94",
@@ -89,9 +90,11 @@ def create_db():
               "d0174cf90b6ec4e26ee2fc013714b0803dec5dd1",
               "3849a35ae6122e0b7a035c2ba66e97b9e4ab9efa" ])
 
+
     # Given mature tag by us
     manual_mature = set(["f24ab6f03d96aada87d4e14b2dac4aa1cee8d787",
-                         "fd4b56c7216c2f96db4b751af68aa2789c327d48"])
+                     "fd4b56c7216c2f96db4b751af68aa2789c327d48",
+                     "ebe983567c5b64970d5dff2fe78dd1573f0d7b61"])
 
     # Grey list (quietly disable link)
     grey_list = set(["ca8cfeb5b6660a0b8874593058178b7ce6af5fed",
