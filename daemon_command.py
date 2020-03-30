a4ae8ba2ab294bcdd01988d25fc7902e8bc9d1d0
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-03-30 08:28:55 +0000

    keep file...yeah yeah

--- daemon_command.py
+++ daemon_command.py
@@ -8,11 +8,12 @@ def daemon_command(command):
     Run a daemon command and return its output.
     """
     command = config.lbrynet_bin + " " + command
-    os.system(command + " > daemon.out 2>&1")
-    time.sleep(5.0)
+    code = os.system(command + " > daemon.out 2>&1")
+#    print(code)
+#    time.sleep(5.0)
     f = open("daemon.out")
     result = json.load(f)
     f.close()
-    os.system("rm daemon.out")
+#    os.system("rm daemon.out")
     return result
 
