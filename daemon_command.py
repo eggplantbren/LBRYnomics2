b1190695b5869ac784dcad64cba3a972bb202f36
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-03-30 07:17:11 +0000

    Terrible hack. Should really use the thing at port 5279

--- daemon_command.py
+++ daemon_command.py
@@ -1,13 +1,15 @@
 import config
 import json
 import os
+import time
 
 def daemon_command(command):
     """
     Run a daemon command and return its output.
     """
     command = config.lbrynet_bin + " " + command
-    os.system(command + " > daemon.out")
+    os.system(command + " > daemon.out 2>&1")
+    time.sleep(5.0)
     f = open("daemon.out")
     result = json.load(f)
     f.close()
