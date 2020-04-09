280a349d957fa394f28f34622cbb5a23ae7fcaf0
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-04-09 19:54:09 +0000

    Handle nonexistent max better

--- recent.py
+++ recent.py
@@ -126,10 +126,14 @@ def count_boosts(now):
             query += "WHERE height >= ?"
             data += (cutoff, )
 
+
+
         for row in dbs["claims"].execute(query, data):
             biggest = row[1]
             result["num_{label}".format(label=labels[i])] = row[0]
-            result["biggest_{label}".format(label=labels[i])] = row[1]/1.0E8
+            if biggest is not None:
+                biggest = biggest/1E8
+            result["biggest_{label}".format(label=labels[i])] = biggest
             break
 
 
@@ -172,11 +176,15 @@ def count_boosts(now):
             query += "AND support.height >= ?"
             data += (cutoff, )
 
+        claim_name, claim_id = None, None
         for row in dbs["claims"].execute(query, data):
             claim_name, claim_id = row[0:2]
 
-        result["tv_url_{label}".format(label=labels[i])] = "https://lbry.tv/" \
-                + claim_name + ":" + claim_id
+        tv_url = None
+        if claim_name is not None:
+            tv_url = "https://lbry.tv/" + claim_name + ":" + claim_id
+        result["tv_url_{label}".format(label=labels[i])] = tv_url
+
 
         # Get NSFW status of max boosted claim
         query = """
