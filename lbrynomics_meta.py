e3dca4df423db0a00ff6f3dd9f94392a0cd99f66
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-04-10 22:22:49 +0000

    First pass

--- lbrynomics_meta.py
+++ lbrynomics_meta.py
@@ -12,12 +12,19 @@ def lbrynomics_meta():
     # Alias
     db = dbs["lbrynomics"]
 
-    # 
+    # Make the measurements!
     result["num_historical_estimates"] = db.execute("""
         SELECT COUNT(id) FROM measurements WHERE lbc_deposits IS NULL;
         """).fetchone()[0]
-#    result["num_actual_measurements"] = 
-#    result["num_daily_channel_measurements"] = 
+    result["num_actual_measurements"] = db.execute("""
+        SELECT count(id) FROM measurements WHERE lbc_deposits IS NOT NULL;
+        """).fetchone()[0]
+    result["num_daily_channel_measurements"] = db.execute("""
+        SELECT count(id) FROM epochs;
+        """).fetchone()[0]
+    result["days_actively_measuring"] = db.execute("""
+        select count(*) from (select cast(time/86400.0 as INTEGER) t, COUNT(lbc_deposits)
+        c from measurements GROUP BY t HAVING c > 0);""").fetchone()[0]
 
     # Write to file
     f = open("json/lbrynomics.json", "w")
