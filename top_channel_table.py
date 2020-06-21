dc7c45b8c67ff069fa8b8db4d0a6abd910f07729
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-06-21 08:03:43 +0000

    Info from claims.db

--- top_channel_table.py
+++ top_channel_table.py
@@ -47,6 +47,56 @@ def create_tables():
 
 
 
+def get_vanity_name(claim_hash):
+    """
+    Get the vanity name of a channel.
+    """
+    rows = cdb.execute("""SELECT claim_name FROM claim
+                          WHERE claim_hash = ?;""", (claim_hash, )).fetchall()
+    result = None
+    if len(rows) == 1:
+        result = rows[0][0]
+    return result
+
+
+def get_lbc(claim_hash):
+    lbc = 0.0
+    rows = cdb.execute("""SELECT (amount + support_amount) FROM claim
+                          WHERE claim_hash = ?;""", (claim_hash, )).fetchall()
+    if len(rows) == 1:
+        lbc += rows[0][0]/1E8
+    rows = cdb.execute("""SELECT SUM(amount + support_amount) FROM claim
+                          WHERE channel_hash=?;""", (claim_hash, )).fetchall()
+    if len(rows) == 1:
+        lbc += rows[0][0]/1E8
+    return lbc
+
+
+def get_reposts(claim_hash):
+    reposts = 0
+    rows = cdb.execute("""SELECT SUM(reposted) FROM claim
+                          WHERE channel_hash=?;""", (claim_hash, )).fetchall()
+    if len(rows) == 1:
+        reposts = rows[0][0]
+    return reposts
+
+
+def get_nsfw(claim_hash):
+    nsfw = False
+    rows = cdb.execute("""SELECT COUNT(*) FROM tag WHERE claim_hash = ?
+                          AND tag.tag IN ('mature', 'xxx', 'sex', 'porn', 'nsfw');""",
+                       (claim_hash, )).fetchall()
+    if len(rows) > 0:
+        nsfw = rows[0][0] > 0
+    return nsfw
+
+
 if __name__ == "__main__":
     create_tables()
+    claim_id = "36b7bd81c1f975878da8cfe2960ed819a1c85bb5"
+    claim_hash = bytes.fromhex(claim_id)[::-1]
+    print(get_vanity_name(claim_hash))
+    print(get_lbc(claim_hash))
+    print(get_reposts(claim_hash))
+    print(get_nsfw(claim_hash))
 
