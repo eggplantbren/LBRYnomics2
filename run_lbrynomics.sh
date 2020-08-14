9862dfc83c534b2e1d5ce89e51706999799114a2
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-14 01:58:59 +0000

    Run script works

--- run_lbrynomics.sh
+++ run_lbrynomics.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 python main.py |& rotatelogs -n 1 ./main.log 1M &
-sleep 10
+python server.py |& rotatelogs -n 1 ./server.log 1M &
 python top_500.py |& rotatelogs -n 1 ./top_500.log 1M &
-sleep 10
 python view_crawler.py |& rotatelogs -n 1 ./view_crawler.log 1M &
 
