68222912ff29dab49838872560cf70f955c43688
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-08-16 21:31:15 +0000

    Streamlined

--- json_to_csv.py
+++ json_to_csv.py
@@ -6,8 +6,14 @@ x = json.load(f)
 f.close()
 
 df = pandas.DataFrame()
-for key in x.keys():
-    df[key] = x[key]
+df["rank"] = x["ranks"]
+df["vanity_name"] = x["vanity_names"]
+df["claim_id"] = x["claim_ids"]
+df["lbc"] = x["lbc"]
+df["followers"] = x["subscribers"]
+df["views"] = x["views"]
+df["reposts"] = x["times_reposted"]
+df["nsfw"] = x["is_nsfw"]
 
 df.to_csv("top_500.csv", header=True, index=False)
 
