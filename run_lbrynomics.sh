135d6f0a8193ddba3639a04bd68bca0b221214d1
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-01-20 09:41:38 +1300

    No server, no nicing for main.py

--- run_lbrynomics.sh
+++ run_lbrynomics.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ionice -c 3 nice -n19 python main.py |& rotatelogs -n 1 ./main.log 1M &
-python server.py |& rotatelogs -n 1 ./server.log 1M &
+python main.py |& rotatelogs -n 1 ./main.log 1M &
 python top_2000.py |& rotatelogs -n 1 ./top_2000.log 1M &
 ionice -c 3 nice -n19 python view_crawler.py |& rotatelogs -n 1 ./view_crawler.log 1M &
 python keepalive.py
