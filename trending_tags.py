155c12ebfeb635b0c8eab4112bd5b8cfc7efe2ac
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-06-30 10:34:01 +1200

    Refactor data

--- trending_tags.py
+++ trending_tags.py
@@ -1,7 +1,9 @@
 import apsw
 from config import *
+import datetime
 import json
 import requests
+import time
 
 def run():
 
@@ -18,16 +20,18 @@ def run():
                                  json=dict(method="status", params={})).json()
         height = response["result"]["wallet"]["blocks"]
 
-
         bin_size = 1000
-        result = []
+        result = {}
+        result["human_time_utc"] = str(datetime.datetime.utcfromtimestamp(time.time()))
+        result["items"] = []
+
         rank = 1
         for row in cdb.execute("SELECT tag, COUNT(*) AS number FROM tag\
                                     WHERE height >= ?\
                                     GROUP BY tag.tag\
                                     ORDER BY number DESC\
                                     LIMIT 200;", (height - bin_size, )):
-            result.append((rank, ) + row)
+            result["items"].append(dict(rank=rank, tag=row[0], count=row[1]))
             rank += 1
 
         f = open("json/popular_tags_last_1000_blocks.json", "w")
